9c69075468905e9651e36e72aacfd6a17275664c
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-02-12 17:09:06 +0000

    summit

--- examples/GenJetAnalysis.C
+++ examples/GenJetAnalysis.C
@@ -13,27 +13,37 @@
 void GenJetAnalysis()
 { 
 
-
   
   int len;
   static Float_t PT = 0;
   TFile *f = new TFile("../delphes_MG_NEW.root");
-  TTree *t = (TTree*)f->Get("Delphes");
+  TTree *fChain = (TTree*)f->Get("Delphes");
 
-  TBranch *j = t->GetBranch("GenJet.PT");
-  t->SetMakeClass(1);
-  t->SetBranchAddress("GenJet",&len);
+  TBranch *j = fChain->GetBranch("GenJet.PT");
+  fChain->SetMakeClass(1);
+  fChain->SetBranchAddress("GenJet",&len);
   j->SetAddress(&PT);
 
 
 
-  Long64_t numberOfEntries = Delphes->GetEntries();
+  Long64_t numberOfEntries = fChain->GetEntries();
 
+
+  cout << "Entries : " << numberOfEntries << endl;
+
+  // Problem here
+
+  for(int k=0; k<numberOfEntries; k++)
+    {
+      fChain->GetEntry(k);
+      cout << "NEW EVENT [" << k << " of " << numberOfEntries << "] " << endl;
+    }
+  
   
   for(int i=0; i<5; i++)
     {
+     
       j->GetEntry(i);
-      
       cout << " PT : " << PT << endl;
 
       
